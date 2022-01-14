1afc1723ae8106846d70cd4c64b03e0418516b55
leroy <bondh185@ucla.edu>
2022-01-14 12:35:53 -0800

    copying and modifying format from simulation for remote submission of multiple datasets

--- real_data_individual_run.R
+++ real_data_individual_run.R
@@ -1,2 +1,132 @@
 ## real_data_individual_run.R
 # run each dataset with each method and record output
+
+library("devtools")
+library("roxygen2")
+library("DMRscaler")
+library("bumphunter")
+library("DMRcate")
+library("doParallel")
+registerDoParallel()
+
+
+output_dir <-paste("./results/")
+
+load("_setup.Rdata")
+
+
+args <- commandArgs(trailingOnly = TRUE)
+if(length(args) < 2){
+  stop("must supply arguments for dataset (DATA_SET) and method parameters (METHOD_SET)")
+}
+
+DATASET_ID <- as.numeric(args[1])
+METHOD_SET_ID <- as.numeric(args[2])
+# filename <- paste(output_dir,"args_test_",DATASET_ID,"__method_set_",METHOD_SET_ID,".csv", sep="" )
+# TEST <- data.frame("DATASET_ID"=DATASET_ID, "METHOD_SET_ID"=METHOD_SET_ID )
+# write.table(TEST, filename, row.names = F)
+
+
+dataset_list <- datasets_list[[DATASET_ID]]
+dataset_name <- names(datasets_list)[DATASET_ID]
+
+method_set <- method_set_list[[METHOD_SET_ID]]
+method_set_name <- names(method_set_list)[METHOD_SET_ID]
+
+
+
+### Start: Set up dataset ####
+g12 <- c(dataset_list$g1,dataset_list$g2)
+g1 <- dataset_list$g1
+g2 <- dataset_list$g2
+B <- dataset_list$B
+
+### End: Set up dataset ####
+
+
+
+method_name <- method_set$method
+
+if(grepl("dmrscaler", method_name, ignore.case = TRUE)){
+  mwr <- DMRscaler::run_MWW(g1,g2,B_mod)
+  locs$pval <- mwr$p_val
+} else if(grepl("bumphunter", method_name, ignore.case = TRUE)){
+  design <- rep(-1,length(colnames(B_mod)))
+  design[which(is.element(colnames(B_mod),g1))] <- 1
+  design <- cbind(rep(1,length(colnames(B_mod) ) ), design )
+  colnames(design) <- c("(Intercept)","(Intercept)")
+  
+} else if(grepl("dmrcate", method_name, ignore.case = TRUE)){
+  design <- rep(-1,length(colnames(B_mod)))
+  design[which(is.element(colnames(B_mod),g1))] <- 1
+  design <- cbind(rep(1,length(colnames(B_mod) ) ), design )
+  colnames(design)<- c("(Intercept)","(Intercept)")
+  M <- log2(B_mod / (1-(B_mod)) )
+  myannotation <- cpg.annotate("array", object=M, what="M", arraytype = "450K", analysis.type = "differential", design = design,  coef = 2)
+  
+} else if(grepl("comb", method_name, ignore.case = TRUE)){
+  mwr <- DMRscaler::run_MWW(g1,g2,B_mod)
+  locs$pval <- mwr$p_val
+  combp_input_bed <- data.frame(chrom=locs$chr,start=locs$pos,end=locs$pos+1,pval=mwr$p_val)
+  combp_input_bed <- combp_input_bed[order(combp_input_bed$chrom),]
+  colnames(combp_input_bed)[1] <- "chrom"
+  combp_input_bed <- combp_input_bed[order(as.character(combp_input_bed$chrom)),]
+  combp_temp_file_prefix <- paste(output_dir,"simul_set_",SIMUL_SET_ID,"__method_set_",METHOD_SET_ID,"_combp",sep="")
+  filename <- paste(combp_temp_file_prefix,"_input.bed",sep="")
+  data.table::fwrite(combp_input_bed, file = filename, row.names = F,col.names = T, sep = "\t")
+  combp_out_filename <- paste(combp_temp_file_prefix,".regions-t.bed",sep="")
+  
+  method_set$function_call <-  paste(method_set$function_call,
+                                     combp_temp_file_prefix,
+                                     filename, "\")",  sep=" " )
+  
+} else {
+  stop("method_name not found")
+}
+
+t1 <- Sys.time()
+method_set_result <- eval(parse(text=method_set$function_call))
+t2 <- Sys.time()
+filename <- paste(output_dir,"simul_set_",SIMUL_SET_ID,"__method_set_",METHOD_SET_ID,"_TIME.csv", sep="" )
+TIME <- data.frame("run"=basename(filename), "time"=as.numeric(difftime(t2,t1, units="secs")))
+write.table(TIME, filename, row.names = F)
+
+
+## convert all method outputs to standard GRange objects with chr,start,stop,pval
+if(grepl("dmrscaler", method_name, ignore.case = TRUE)){
+  out_df <- method_set_result[[1]][0,]
+  for(i in 1:length(method_set_result)){
+    if(nrow(method_set_result[[i]])>0 ){
+      out_df <- rbind(out_df, data.frame(method_set_result[[i]],layer=names(method_set_result)[i] ))
+    }
+  }
+  
+} else if(grepl("bumphunter", method_name, ignore.case = TRUE)){
+  out_df <- method_set_result$table
+  colnames(out_df)[which(colnames(out_df)=="end")] <- "stop"
+  colnames(out_df)[which(colnames(out_df)=="p.value")] <- "pval_region"
+  
+} else if(grepl("dmrcate", method_name, ignore.case = TRUE)){
+  out_df <- data.frame(coord=method_set_result@coord,
+                       stouffer=method_set_result@Stouffer,
+                       HMFDR=method_set_result@HMFDR,
+                       Fisher=method_set_result@Fisher)
+  out_df$chr <- stringr::str_split_fixed(out_df$coord,":",2)[,1]
+  out_df$start <- stringr::str_split_fixed(stringr::str_split_fixed(out_df$coord,":",2)[,2],"-",2)[,1]
+  out_df$stop <- stringr::str_split_fixed(stringr::str_split_fixed(out_df$coord,":",2)[,2],"-",2)[,2]
+  out_df$pval_region <- out_df$stouffer
+  
+} else if(grepl("comb", method_name, ignore.case = TRUE)){
+  out_df <- data.table::fread(combp_out_filename)
+  colnames(out_df)[which(colnames(out_df)=="end")] <- "stop"
+  colnames(out_df)[which(colnames(out_df)=="#chrom")] <- "chr"
+  colnames(out_df)[which(colnames(out_df)=="z_p")] <- "pval_region"
+  ## add line removing combp intermediary files
+  system(paste("rm ", paste(combp_temp_file_prefix,"*",sep = "") ))
+  
+} else {
+  stop("method_name not found")
+}
+
+filename <- paste(output_dir,"simul_set_",SIMUL_SET_ID,"__method_set_",METHOD_SET_ID,"_result.csv", sep="" )
+write.table(out_df, file = filename, row.names = F)
--- real_data_setup.R
+++ real_data_setup.R
@@ -1,2 +1,91 @@
 ## real_data_setup.R ##
 # perform preprocessing on all datasets and set up method sets
+
+
+## install DMRscaler
+library("devtools")
+library("roxygen2")
+install("../DMRscaler", quick=T)
+
+library(doParallel)
+registerDoParallel()
+
+results_dir <-paste("./results/")
+
+
+## Example Dataset Setup
+##We will use data from GSE149960 from (REFERENCE) with DNA methylation from fibroblasts from progeria patients and controls measured on the Illumina methylation EPIC array.
+
+library("GEOquery")
+
+
+
+# Pre-processing
+###  Reading of idat files done with minfi library ###
+
+gse <- getGEO("GSE74432", GSEMatrix = TRUE)
+phen <- gse$GSE74432_series_matrix.txt.gz@phenoData@data
+phen <- phen[intersect( grep("[Cc]ontrol", phen$characteristics_ch1.1),grep("whole blood",phen$characteristics_ch1.2) ),]
+rm(gse)
+
+## get methylation data as idat files (NOTE: this saves files locally in working directory, unpacked size is 2.01 Gb
+if(length(list.files("GSE74432/idat", pattern = "idat$"))==0){
+  getGEOSuppFiles("GSE74432")
+  untar("GSE74432/GSE74432_RAW.tar", exdir = "GSE74432/idat")
+  file.remove("GSE74432/GSE74432_RAW.tar")
+  idat_files <- list.files("GSE74432/idat", pattern = "idat.gz$", full = TRUE)
+  sapply(idat_files, gunzip, overwrite = TRUE); rm(idat_files)
+}
+
+##  Preprocessing
+library("minfi")
+
+
+###  Reading of idat files done with minfi library ###
+idats_dir<-"GSE74432/idat"
+targets <- data.frame("Basename"= stringr::str_split_fixed(basename(phen$supplementary_file), "_Grn", 2)[,1] )
+RGSet <- read.metharray.exp(base = idats_dir, targets = targets)
+GRset.funnorm <- preprocessFunnorm(RGSet);rm(RGSet)
+snps <- getSnpInfo(object = GRset.funnorm)
+GRset.funnorm <- dropLociWithSnps(GRset.funnorm, snps=c("SBE", "CpG"), maf=0);rm(snps)
+rm(idats_dir)
+
+
+controls <- grep("[Cc]ontrol",phen$title)
+locs <- getLocations(GRset.funnorm)
+locs <- data.frame("names"=locs@ranges@NAMES, "pos"=locs@ranges@start, "chr" = rep(locs@seqnames@values, locs@seqnames@lengths))
+B <- getBeta(GRset.funnorm)
+
+
+method_set_list <- list(
+  dmrscaler_1 = list(method="dmrscaler",
+                     function_call=" DMRscaler::dmrscaler(locs = locs, locs_pval_cutoff = 0.01, region_signif_method = \"bon\", region_signif_cutoff = 0.01, window_type = \"k_near\", window_sizes = c(2,4,8,16,32,64), output_type = \"comp\") " ),
+  dmrscaler_2 = list(method="dmrscaler",
+                     function_call=" DMRscaler::dmrscaler(locs = locs, locs_pval_cutoff = 0.01, region_signif_method = \"ben\", region_signif_cutoff = 0.01, window_type = \"k_near\", window_sizes = c(2,4,8,16,32,64), output_type = \"comp\") " ),
+  bumphunter_1 = list(method="bumphunter",
+                      function_call="bumphunter(B_mod,as.matrix(design),chr = locs$chr, pos=locs$pos, cutoff=0.1, maxGap=1e3, B=250, smoothFunction=loessByCluster )"),
+  bumphunter_2 = list(method="bumphunter",
+                      function_call="bumphunter(B_mod,as.matrix(design),chr = locs$chr, pos=locs$pos, cutoff=0.1, maxGap=1e6, B=250, smoothFunction=loessByCluster )"),
+  dmrcate_1 = list(method="dmrcate",
+                   function_call="dmrcate(myannotation, lambda=1e3, C=2)"),
+  dmrcate_2 = list(method="dmrcate",
+                   function_call="dmrcate(myannotation, lambda=1e6, C=2000)"),
+  combp_1 = list(method="combp",
+                 function_call="system(\"comb-p pipeline -c 4 --dist 1000 --step 100 --seed 1e-3 --region-filter-p 0.1 -p " ),
+  combp_2 = list(method="combp",
+                 function_call="system(\"comb-p pipeline -c 4 --dist 10000 --step 5000 --seed 1e-3 --region-filter-p 0.1 -p " )
+)
+
+
+## write Rdata objects to load into each real_data_individual_run.R script
+filename <- paste("real_data_setup.Rdata")
+save.image(file=filename)
+## write dataset_table
+dataset_table <- names(dataset_list)
+filename <- paste("dataset_table.csv")
+write.table(dataset_table, filename, row.names = F, col.names=F, sep=",")
+
+## write method_table
+method_table <- names(method_set_list)
+filename <- paste("method_table.csv")
+write.table(method_table, filename, row.names = F, col.names=F, sep=",")
